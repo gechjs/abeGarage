32eb487922dd8da096bf1a77e6f9c4f8265dc484
gechjs <gizachew980@gmail.com>
2025-04-13 18:20:59 +0300

    controller for employee added

--- backend/controllers/employee.controller.js
+++ backend/controllers/employee.controller.js
@@ -1,7 +1,13 @@
+// Import the employee service 
 const employeeService = require('../services/employee.service');
+// Create the add employee controller
 async function createEmployee(req, res, next) {
 
+  // console.log(req.headers); 
+
+  // Check if employee email already exists in the database 
   const employeeExists = await employeeService.checkIfEmployeeExists(req.body.employee_email);
+  // If employee exists, send a response to the client
   if (employeeExists) {
     res.status(400).json({
       error: "This email address is already associated with another employee!"
@@ -9,6 +15,7 @@ async function createEmployee(req, res, next) {
   } else {
     try {
       const employeeData = req.body;
+      // Create the employee
       const employee = await employeeService.createEmployee(employeeData);
       if (!employee) {
         res.status(400).json({
@@ -28,6 +35,25 @@ async function createEmployee(req, res, next) {
   }
 }
 
+// Create the getAllEmployees controller 
+async function getAllEmployees(req, res, next) {
+  // Call the getAllEmployees method from the employee service 
+  const employees = await employeeService.getAllEmployees();
+  // console.log(employees);
+  if (!employees) {
+    res.status(400).json({
+      error: "Failed to get all employees!"
+    });
+  } else {
+    res.status(200).json({
+      status: "success",
+      data: employees,
+    });
+  }
+}
+
+// Export the createEmployee controller 
 module.exports = {
-  createEmployee
+  createEmployee,
+  getAllEmployees
 };
